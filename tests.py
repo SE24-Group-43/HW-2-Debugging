6a957d814641e4ad8f3911287832ca26e4ea0fd7
LavanyaMiddha <49492261+LavanyaMiddha@users.noreply.github.com>
2024-09-12 20:12:34 +0000

    adding test cases

--- tests.py
+++ tests.py
@@ -1,1 +1,18 @@
+import pytest
+from hw2_debugging import merge_sort
 
+a1=[9,8,7,6,5,4,3,2,0]
+a2=[-9,-2,-3,-1,-10,-11]
+a3=[1,2,3,4,5,6,0,10]
+
+def test1_merge_sort():
+  b1=sorted(a1)
+  assert merge_sort(a1) == b1
+
+def test2_merge_sort():
+  b2=sorted(a2)
+  assert merge_sort(a2) == b2
+
+def test3_merge_sort():
+  b3=sorted(a3)
+  assert merge_sort(a3) == b3
