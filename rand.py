1b9fe72618a53afc520a3cc6e9d7e20fc4c30f34
LavanyaMiddha <49492261+LavanyaMiddha@users.noreply.github.com>
2024-09-12 18:09:54 +0000

    added changes for pylint

--- rand.py
+++ rand.py
@@ -1,8 +1,10 @@
+'''Python code to create an array with random elments'''
 import subprocess
 
 def random_array(arr):
+    '''This function creates a random array'''
     shuffled_num = None
-    for i in range(len(arr)):
-        shuffled_num = subprocess.run(["shuf", "-i1-20", "-n1"], capture_output=True)
+    for i,_ in enumerate(arr):
+        shuffled_num = subprocess.run(["shuf", "-i1-20", "-n1"], capture_output=True, check=False)
         arr[i] = int(shuffled_num.stdout)
     return arr
